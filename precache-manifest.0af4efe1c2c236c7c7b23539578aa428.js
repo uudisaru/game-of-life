8dd3201adadecac514f143d96704ca32d79d7332
Toomas Uudisaru <toomas.uudisaru@aktors.ee>
2020-04-26 17:51:26 +0300

    Updates

---
+++ precache-manifest.0af4efe1c2c236c7c7b23539578aa428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf093300e99ab590b1990a6ce15ba834",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "9ae9ef5b70259fec365c",
+    "url": "/game-of-life/static/css/main.7eba0433.chunk.css"
+  },
+  {
+    "revision": "cb6a882802607d7073ad",
+    "url": "/game-of-life/static/js/2.24f3f31e.chunk.js"
+  },
+  {
+    "revision": "2aff8cf35e0d5ba1e4add0f1b477519f",
+    "url": "/game-of-life/static/js/2.24f3f31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae9ef5b70259fec365c",
+    "url": "/game-of-life/static/js/main.303efa77.chunk.js"
+  },
+  {
+    "revision": "51df4f6466fc46c339a1",
+    "url": "/game-of-life/static/js/runtime-main.622907b2.js"
+  }
+]);
